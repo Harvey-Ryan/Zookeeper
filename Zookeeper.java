aeca752f77fdc012f6c43bf4ed5e4dfa5d72cd7f
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-08-13 16:11:51 -0500

    Completed

---
+++ Zookeeper.java
@@ -0,0 +1,17 @@
+
+
+// Every mammal should have an energy level that starts at 100.
+// Each animal's energy level should be displayed with the displayEnergy method,
+// which should print and return the energy level.
+
+
+// throwSomething() - decrease the gorilla's energy by 5 and print out a message indicating that the gorilla has thrown something
+// eatBananas() - increase the gorilla's energy by 10 and print out a message indicating the gorilla's satisfaction
+// climb() - decrease the gorilla's energy by 10 and print out a message indicating the gorilla has climbed a tree
+
+
+
+// Bat: its energy level is 300
+// fly() - decrease the bat's energy by 50 and print a message indicating the bat is airborne
+// eatHumans() - increase the bat's energy by 25 and print a message indicating the bat's satisfaction
+// attackTown() - decrease the bat's energy by 100 and print a message describing the bat's attack
